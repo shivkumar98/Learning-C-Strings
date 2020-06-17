5cfd21b7ab0b4da1d0239f4813e786ba6e3e4c2d
shivkumar98 <shiv_kumar.98@outlook.com>
2020-06-17 13:04:23 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.CompilerServices;
+using System.Xml.Schema;
+
+namespace Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sentence = "this is a ridiculously long piece of text which noone will ever read";
+            var summary = StringUtitlity.SummariseText(sentence);
+            Console.WriteLine(summary);
+
+        }
+
+        
+    }
+
+
+}
---
+++ StringUtitlity.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Strings
+{
+    public class StringUtitlity
+    {
+        public static string SummariseText(string text, int maxLength = 20)
+        {
+
+
+            if (text.Length < maxLength)
+            {
+                return text;
+            }
+            else
+            {
+                var words = text.Split(' ');
+                var totlalChars = 0;
+                var summaryWords = new List<string>();
+
+                foreach (var word in words)
+                {
+                    summaryWords.Add(word);
+                    totlalChars += word.Length + 1;
+                    if (totlalChars > maxLength)
+                    {
+                        break;
+                    }
+                }
+
+                var summary = String.Join(" ", summaryWords) + "...";
+                return summary;
+            }
+        }
+
+    }
+
+}
